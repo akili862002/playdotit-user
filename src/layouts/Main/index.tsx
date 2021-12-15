882bde55e218e9eb468b69ea19e36650b211b609
“Surtin” <surtin@usumsoftware.com>
2021-12-15 07:00:43 +0700

    update link

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -9,7 +9,8 @@ const MainLayout: React.FC<IMainLayoutProps> = ({ children }) => {
       <main className="w-full h-full max-w-md px-2 pb-5">{children}</main>
 
       <p className="absolute text-sm bottom-1 left-1">
-        © Play.it is designed and created by <a href="">Dung Nguyen</a>*,{" "}
+        © Play.it is designed and created by{" "}
+        <a href="https://www.facebook.com/rengar.the/">Dung Nguyen</a>*,{" "}
         <a href="">Quoc Duy</a>, Quoc Anh and Phuoc Toan
       </p>
     </>
